a7efeb98396bf8f0e276632098d93cac1fa11e4a
seacasa <seacasamemes@gmail.com>
2025-11-01 00:22:14 -0700

    Update abis.ts

--- app/lib/abis.ts
+++ app/lib/abis.ts
@@ -1,1 +1,23 @@
+export const erc20Abi = [
+  {
+    name: 'transfer',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [{ name: 'to', type: 'address' }, { name: 'value', type: 'uint256' }],
+    outputs: [{ name: '', type: 'bool' }],
+  },
+] as const;
 
+export const erc721Abi = [
+  {
+    name: 'safeTransferFrom',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'from', type: 'address' },
+      { name: 'to', type: 'address' },
+      { name: 'tokenId', type: 'uint256' },
+    ],
+    outputs: [],
+  },
+] as const;
